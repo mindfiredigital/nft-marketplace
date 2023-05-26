6faf3b06159c58080c48a9a3c89a56c541fcdf50
Bhupesh-MS <bhupeshd@mindfiresolutions.com>
2023-05-26 19:07:11 +0530

    Siddharth's mint page UI

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 function Footer() {
     return (
         <div className="footer flex flex-row justify-center py-6 bg-black text-white">
-            <div className="basis-1/1">
+            <div className="basis-1/1 text-center">
                 <span className="text-sm font-light">
                     Copyright © 2023 Fandom | NFT Marketplace. Designed by <b>Mindfire</b>
                 </span>
